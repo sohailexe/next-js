0d5d5107676b24319a2d3bcab85541dfa1e08a03
Sohail Faiz Rasool <sa472811@gmail.com>
2024-10-06 07:56:48 +0500

    server component

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <div>cabins</div>;
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Cabins Page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>
+            <p>{user.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import Navigation from "./components/Navigation";
-
+import Logo from "./components/Logo";
 export const metadata = {
   title: "Sohail Faiz",
 };
@@ -8,6 +8,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Logo />
         <Navigation />
         <main>{children}</main>
         <footer>Copy Right by Sohail Faiz</footer>
