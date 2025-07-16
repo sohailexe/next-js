a9904838b73966157da00afa409a67bf6494bb43
sohailexe <div.sohailfaiz@gmail.com>
2025-07-17 02:16:46 +0500

    old way of fetching data

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Old from "./components/Old";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <h1>Welcome to the New Page</h1>
+      <Old />
+    </div>
+  );
 };
 
 export default page;
